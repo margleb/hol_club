643ad8f0ffa7a4ffa75ecdaf513a33c68b881610
margleb <margleb.dm@gmail.com>
2026-01-29 16:04:22 +0300

    fix: переопределиен в env чатов

--- config/config.py
+++ config/config.py
@@ -3,7 +3,8 @@ from dynaconf import Dynaconf
 settings = Dynaconf(
     envvar_prefix=False,  # "DYNACONF",
     environments=True,  # Автоматически использовать секцию текущей среды
-    env_switcher="ENV_FOR_DYNACONF", 
+    env_switcher="ENV_FOR_DYNACONF",
+    env_nested_delimiter="__",
     settings_files=['settings.toml', '.secrets.toml'],
     load_dotenv=True
 )
